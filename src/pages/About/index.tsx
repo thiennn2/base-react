5666ca5d9f657a62b5eb182f2afd80465b39598b
thiennn <thien.nguyenngoc@amela.vn>
2022-07-29 16:14:05 +0700

    feature: add AppContext

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,17 +1,25 @@
-import React from "react";
-import {Link} from 'react-router-dom';
-import {useTranslation} from 'react-i18next';
+import React, { useContext } from "react";
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { AppContext } from "store";
 
 export default function About() {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
+  const { posts } = useContext(AppContext);
   return (
     <div>
+      <Link to="/">Home</Link>
       <h1>About</h1>
       <p>
         This is an example of how you can do things like this:
       </p>
       <p>{t("common.helloWorld")}</p>
-      <Link to="/">Home</Link>
+      {posts.map(post => (
+        <article className="post-excerpt" key={post.id}>
+          <h3>{post.title}</h3>
+          <p className="post-content">{post.content.substring(0, 100)}</p>
+        </article>
+      ))}
     </div>
   )
 }
---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import {createContext} from 'react';
+
+export interface AppContextProps {
+  posts: any[];
+}
+
+const initialContext: AppContextProps = {
+  posts: [],
+};
+
+export const AppContext = createContext(initialContext);
