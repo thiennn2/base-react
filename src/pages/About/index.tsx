2ba1632e32267dd9352373f2d7913488a040a791
thiennn <thien.nguyenngoc@amela.vn>
2022-07-29 10:53:19 +0700

    feature: i18n

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import {useTranslation} from 'react-i18next';
 
 export default function About() {
+  const {t} = useTranslation();
   return (
     <div>
       <h1>About</h1>
       <p>
         This is an example of how you can do things like this:
       </p>
+      <p>{t("common.helloWorld")}</p>
       <Link to="/">Home</Link>
     </div>
   )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {useTranslation} from 'react-i18next';
 
 export default function Home() {
+  const {t} = useTranslation();
   return (
     <div>
       <h1>Home</h1>
       <p>
         This is an example of how you can do things like this:
       </p>
+      <p>{t("common.helloWorld")}</p>
       <Link to="/about">About</Link>
     </div>
   )
