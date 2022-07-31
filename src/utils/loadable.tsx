5d8f925a5225812552d6b68b29cb30fd041da905
thiennn <thien.nguyenngoc@amela.vn>
2022-07-31 08:23:05 +0700

    feat: add commitlint

--- src/utils/loadable.tsx
+++ src/utils/loadable.tsx
@@ -3,7 +3,7 @@ import React, { lazy, Suspense } from 'react';
 const loadable = (
   importFunc: () => Promise<{ default: React.ComponentType<any> }>,
   { fallback = null } = { fallback: null }
-) => {
+)  => {
   const LazyComponent = lazy(importFunc);
 
   return (props: any) => (
